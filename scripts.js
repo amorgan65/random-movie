91f252aa2c1d9dbe9434481a2ab626a1c23aaf0c
Alec Morgan <alec.morgan@berkeley.edu>
2023-06-05 23:56:48 -0700

    Added background image and styling

--- scripts.js
+++ scripts.js
@@ -1,12 +1,20 @@
-//TODO use javascript to generate genre buttons?
-const GENRES = ['action', 'comedy', 'horror']
+//TODO get array of genres from API call?
+const GENRES = ["action", "comedy", "horror"];
+
+var selectedGenre = "";
 
 window.onload = () => {
-    var genresDiv = document.getElementById("genres")
-    
-    GENRES.forEach(genre => {
-        const btn = document.createElement("button")
-        btn.innerHTML = genre
-        genresDiv.appendChild(btn)
-    })
-}
+  var genresDiv = document.getElementById("genres");
+
+  GENRES.forEach((genre) => {
+    const btn = document.createElement("button");
+    btn.innerHTML = genre;
+    genresDiv.appendChild(btn);
+  });
+};
+
+//TODO on genre button click, save to variable
+
+/* TODO on search button click, use saved variable for 
+    api call? then, dynamically create new page, present
+     movie details */
