2d8884765e17a70620022f372a9ce915b76c0799
Alec Morgan <alec.morgan@berkeley.edu>
2023-06-06 10:47:18 -0700

    Clicking genre button stores selected genre as variable, for search button to use for result

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 //TODO get array of genres from API call?
 const GENRES = ["action", "comedy", "horror"];
 
-var selectedGenre = "";
+let selectedGenre;
 
 window.onload = () => {
   var genresDiv = document.getElementById("genres");
@@ -9,20 +9,24 @@ window.onload = () => {
   GENRES.forEach((genre) => {
     const btn = document.createElement("button");
     btn.setAttribute("data-genre", genre);
+    btn.setAttribute('value', genre)
     btn.innerHTML = genre;
     genresDiv.appendChild(btn);
+    btn.addEventListener("click", (e) => {
+        let x = e.target.value;
+        selectedGenre = x;
+    });
   });
 };
 
-//const container = document.getElementById("genres");
-const genreButtons = document.querySelectorAll("[data-genre]");
-genreButtons.forEach((button) => {
-  button.addEventListener("click", () => {
-    selectedGenre = button.innerHTML
-  });
-});
-
 window.addEventListener("DOMContentLoaded", (event) => {
+    const genreButtons = document.querySelectorAll("[data-genre]");
+    genreButtons.forEach((button) => {
+      button.addEventListener("click", () => {
+        selectedGenre = button.innerHTML
+      });
+    });
+
   var searchButton = document.getElementById("search");
   searchButton.addEventListener("click", () => {
     searchButton.innerHTML = selectedGenre;
