e60398c5ce8c73d11b4e0ea83e09ad33dcbc6986
Alec Morgan <alec.morgan@berkeley.edu>
2023-06-06 01:21:51 -0700

    Adjusted styling of search bar and added event listeners for button clicks, not finished

--- scripts.js
+++ scripts.js
@@ -8,11 +8,31 @@ window.onload = () => {
 
   GENRES.forEach((genre) => {
     const btn = document.createElement("button");
+    btn.setAttribute("data-genre", genre);
     btn.innerHTML = genre;
     genresDiv.appendChild(btn);
   });
 };
 
+//const container = document.getElementById("genres");
+const genreButtons = document.querySelectorAll("[data-genre]");
+genreButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    selectedGenre = button.innerHTML
+  });
+});
+
+window.addEventListener("DOMContentLoaded", (event) => {
+  var searchButton = document.getElementById("search");
+  searchButton.addEventListener("click", () => {
+    searchButton.innerHTML = selectedGenre;
+  });
+});
+
+/*
+function generateMovie(genre) {
+  
+} */
 //TODO on genre button click, save to variable
 
 /* TODO on search button click, use saved variable for 
